3bafc191e543914131df6ca4c1f762715019cc54
Gina Medina <ga.medina0613@gmail.com>
2023-09-18 21:26:16 +0100

    Now with the error tests

--- tests/test_present.py
+++ tests/test_present.py
@@ -15,3 +15,18 @@ def test_present_unwraping():
     gift.wrap("Something")
     destapado = gift.unwrap()
     assert destapado == "Something"
+
+def test_present_has_been_wrap():
+    gift = Present()
+    gift.wrap("Legal")
+    with pytest.raises(Exception) as e:
+        gift.wrap("Sorpresa!")
+    error_message = str(e.value)
+    assert error_message == "A contents has alredy been wrapped"
+
+def test_present_has_not_been_wrap():
+    gift = Present()
+    with pytest.raises(Exception) as e:
+        gift.unwrap()
+    error_message = str(e.value)
+    assert error_message == "No contents have been wrapped."
